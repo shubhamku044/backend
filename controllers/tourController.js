45ae676946fe7df6b7c0401b089633a6d18d741e
Shubham Sharma <shubhamku044@gmail.com>
2022-06-19 09:30:44 +0530

    added custom fields option

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,6 +23,15 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(',').join(' ');
       console.log(sortBy);
       query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     const tours = await query;
